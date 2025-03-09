066edf6e39148876c34f652e7787757136bbea35
Alex Fuller <boberfly@gmail.com>
2025-03-10 01:53:54 +1100

    Arnold : Arnold Operators

--- bin/gaffer.cmd
+++ bin/gaffer.cmd
@@ -11,6 +11,11 @@ setlocal EnableDelayedExpansion
 set GAFFER_ROOT=%~dp0%..
 set "GAFFER_ROOT=%GAFFER_ROOT:\=/%"
 
+if "%GAFFER_PYTHON%" EQU "" (
+	set GAFFER_PYTHON="%GAFFER_ROOT%"\bin\python.exe
+	set PYTHONHOME=%GAFFER_ROOT%
+)
+
 set "HOME=%USERPROFILE:\=/%"
 
 set GAFFER_JEMALLOC=0
@@ -46,14 +51,16 @@ call :appendToPath "%GAFFER_ROOT%\startup" CORTEX_STARTUP_PATHS
 
 call :prependToPath "%GAFFER_ROOT%\graphics" GAFFERUI_IMAGE_PATHS
 
-set OSLHOME=%GAFFER_ROOT%
+if "%OSL_ROOT%" EQU "" (
+	set OSLHOME=%GAFFER_ROOT%
+) else (
+	set OSLHOME=%OSL_ROOT%
+)
 
 call :prependToPath "%USERPROFILE%\gaffer\shaders;%GAFFER_ROOT%\shaders" OSL_SHADER_PATHS
 
 set GAFFEROSL_CODE_DIRECTORY=%USERPROFILE%\gaffer\oslCode
 call :prependToPath %GAFFEROSL_CODE_DIRECTORY% PATH
-
-set PYTHONHOME=%GAFFER_ROOT%
 
 call :prependToPath "%GAFFER_ROOT%\python" PYTHONPATH
 
@@ -183,9 +190,9 @@ for /f "tokens=1* delims=;" %%A in ("%EXTENSION_PATH%") do (
 )
 
 if "%GAFFER_DEBUG%" NEQ "" (
-	%GAFFER_DEBUGGER% "%GAFFER_ROOT%"\bin\python.exe "%GAFFER_ROOT%"/bin/_gaffer.py %*
+	%GAFFER_DEBUGGER% "%GAFFER_PYTHON%" "%GAFFER_ROOT%"/bin/__gaffer.py %*
 ) else (
-	"%GAFFER_ROOT%"\bin\python.exe "%GAFFER_ROOT%"/bin/_gaffer.py %*
+	"%GAFFER_PYTHON%" "%GAFFER_ROOT%"/bin/__gaffer.py %*
 )
 
 ENDLOCAL
