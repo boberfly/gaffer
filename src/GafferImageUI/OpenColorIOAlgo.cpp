2f60c360106de0511e67ac991edf61c5bbd3d721
Murray Stevenson <50844517+murraystevenson@users.noreply.github.com>
2025-05-12 13:11:48 -0700

    OpenColorIOAlgo : Support OCIO 2.3
    This appears to be enough to allow Gaffer to build with OCIO 2.3, while maintaining compatibility with OCIO 2.2.
    
    (cherry picked from commit 837d13db601e221b42e113a7665b2ae41434e44a)

--- src/GafferImageUI/OpenColorIOAlgo.cpp
+++ src/GafferImageUI/OpenColorIOAlgo.cpp
@@ -347,7 +347,12 @@ IECoreGL::Shader::SetupPtr displayTransformToFramebufferShader( const OCIO_NAMES
 			unsigned height = 0;
 			OCIO_NAMESPACE::GpuShaderDesc::TextureType channel = OCIO_NAMESPACE::GpuShaderDesc::TEXTURE_RGB_CHANNEL;
 			OCIO_NAMESPACE::Interpolation interpolation = OCIO_NAMESPACE::INTERP_LINEAR;
+#if OCIO_VERSION_HEX >= 0x02030000
+			OCIO_NAMESPACE::GpuShaderDesc::TextureDimensions dimensions;
+			shaderDesc->getTexture(idx, textureName, samplerName, width, height, channel, dimensions, interpolation);
+#else
 			shaderDesc->getTexture(idx, textureName, samplerName, width, height, channel, interpolation);
+#endif
 			if (
 				!textureName || !*textureName || !samplerName || !*samplerName || width == 0
 			)
